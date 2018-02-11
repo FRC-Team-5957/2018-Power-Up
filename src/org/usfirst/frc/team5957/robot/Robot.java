7c3e443d5d8b01ef5292974e82d99f9b831e9285
astan54321 <astan54321@gmail.com>
2018-02-11 00:46:21 -0500

    Cleaning and added autonomout skeleton

--- src/org/usfirst/frc/team5957/robot/Robot.java
+++ src/org/usfirst/frc/team5957/robot/Robot.java
@@ -10,6 +10,7 @@ package org.usfirst.frc.team5957.robot;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
@@ -36,9 +37,6 @@ public class Robot extends IterativeRobot {
 	final int rearRightCh = 3;
 	final int gearCh = 0;
 
-	// Encoders
-	Encoder leftEnc, rightEnc;
-
 	// Lift and gripper
 	VictorSP leftSpinny, rightSpinny, lift;
 	DigitalInput topLimit, lowLimit;
@@ -59,7 +57,8 @@ public class Robot extends IterativeRobot {
 
 	// Sensors and subsystems
 	ADXRS450_Gyro gyro;
-	DigitalInput leftAuto, rightAuto;
+	Encoder leftEnc, rightEnc;
+	DigitalInput leftAuto, rightAuto, scale;
 	Compressor compressor;
 
 	// CAN Channels
@@ -84,11 +83,14 @@ public class Robot extends IterativeRobot {
 	final int R1 = 6;
 	final int L2 = 2;
 	final int R2 = 3;
-	// TODO: finish mapping for PS4 controller
 	// Logitech
 	// TODO: figure out mapping for Logitech Controller
 	// N64
 	// TODO: figure out mapping for N64 controller
+
+	// Constants and variables
+	String gameData;
+	String startPosition;
 
 	@Override
 	public void robotInit() {
@@ -101,11 +103,7 @@ public class Robot extends IterativeRobot {
 		drive = new DifferentialDrive(new SpeedControllerGroup(frontLeft, rearLeft),
 				new SpeedControllerGroup(frontRight, rearRight));
 		gear = new Solenoid(gearCh);
-		gear.set(lowGear);
-
-		// Encoders
-		leftEnc = new Encoder(2, 3, false, Encoder.EncodingType.k1X);// TODO adjust values until its 1:1
-		rightEnc = new Encoder(4, 5, false, Encoder.EncodingType.k1X);// TODO adjust values until its 1:1
+		gear.set(lowGear); // Setting initial gear
 
 		// Lift and gripper
 		lift = new VictorSP(liftCh);
@@ -122,8 +120,12 @@ public class Robot extends IterativeRobot {
 
 		// Sensors and subsystems
 		gyro = new ADXRS450_Gyro();
+		leftEnc = new Encoder(2, 3, false, Encoder.EncodingType.k1X);// TODO adjust values until its 1 rotation = 1
+																		// count
+		rightEnc = new Encoder(4, 5, false, Encoder.EncodingType.k1X);
 		leftAuto = new DigitalInput(0);
 		rightAuto = new DigitalInput(1);
+		scale = new DigitalInput(2);
 		compressor = new Compressor(PCM);
 
 		// OI
@@ -135,44 +137,66 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void autonomousInit() {
+		// Reset variables
+		gyro.reset();
+		leftEnc.reset();
+		rightEnc.reset();
 
-		// Switch for selecting auto side
-		String autoPosition = getAutoPosition();
-		switch (autoPosition) {
-		case "left":
-			// TODO: Left auto logic (write left auto here)
-			break;
-		case "right":
-			// TODO: Right auto logic (write right auto here)
-			break;
-		case "middle":
-			// TODO: Middle auto logic (write middle auto here)
-			break;
+		// Get data
+		gameData = DriverStation.getInstance().getGameSpecificMessage();
+		startPosition = getStartPosition();
+
+		// Auto logic
+		if (scale.get() && canScoreScale()) { // Scale Auto
+			// move forward
+			// turn based on position
+			// switch to low gear
+			// move towards scale
+			// move lift to correct height
+			// output cube
+		} else if (!scale.get() && canScoreSwitch()) { // Switch Auto
+			if (startPosition == "middle") {
+				// turn based on gameData
+				// move forward
+				// turn toward switch based on gameData
+				// move forward until close to switch
+				// output cube
+			} else {
+				// turn towards switch
+				// move forward until close to switch
+				// output cube
+			}
+		} else { // Forward auto
+			// move forward to break auto line
 		}
 
 	}
 
 	@Override
 	public void autonomousPeriodic() {
-
+		Timer.delay(0.01);
 	}
 
 	@Override
 	public void teleopPeriodic() {
-
 		// TODO: Finish teleop outline
 
-		// Shift operation
+		// Shift
 		if (driver.getRawButton(L1)) {
 			gear.set(lowGear);
 		} else if (driver.getRawButton(R2)) {
 			gear.set(highGear);
 		}
 
+		// Lift logic
+
+		// Intake wheels
+
+		// Climb
+
 		// Standard drive, squared inputs, no correction
 		drive.arcadeDrive(driver.getRawAxis(LY), driver.getRawAxis(RX), true);
 
-		// Standard timer delay
 		Timer.delay(0.01);
 	}
 
@@ -182,28 +206,42 @@ public class Robot extends IterativeRobot {
 	// Code runs periodically at all times
 	@Override
 	public void robotPeriodic() {
-
 	}
 
 	// Methods to be tested
-	public String getAutoPosition() {
-
-		/*
-		 * 3-position switch left position { DIO0 = true, DIO1 = false} right position {
-		 * DIO0 = false, DIO1 = true} middle potitiom { DIO0 = DIO1 = false}
-		 */
-
-		String autoPosition = "";
+	public String getStartPosition() {
+		// 3-position switch
+		// Left position DIO0 = true, DIO1 = false
+		// Right position DIO0 = false, DIO1 = true
+		// Middle potition DIO0 = DIO1 = true
+		// returns auto to run based on match between position switch and game data
+		String auto;
 		if (leftAuto.get() && !rightAuto.get()) {
-			autoPosition = "left";
+			auto = "left";
 		} else if (!leftAuto.get() && rightAuto.get()) {
-			autoPosition = "right";
+			auto = "right";
 		} else {
-			autoPosition = "middle";
+			auto = "middle";
 		}
+		return auto;
+	}
 
-		return autoPosition;
+	public boolean canScoreScale() {
+		if (startPosition != "middle") {
+			return startPosition == "left" && gameData.charAt(1) == 'L'
+					|| startPosition == "right" && gameData.charAt(1) == 'R';
+		} else {
+			return false;
+		}
+	}
 
+	public boolean canScoreSwitch() {
+		if (startPosition == "middle") {
+			return true;
+		} else {
+			return startPosition == "left" && gameData.charAt(0) == 'L'
+					|| startPosition == "right" && gameData.charAt(0) == 'R';
+		}
 	}
 
 	// Test variables
@@ -219,50 +257,60 @@ public class Robot extends IterativeRobot {
 	boolean stoppedL = true;
 	boolean stoppedR = true;
 
+	boolean testPeriodicFunctions = true;
+	boolean testInitFunctions = false;
+
 	@Override
 	public void testInit() {
-
+		if (testInitFunctions) {
+			// Write testInit code in this loop
+		}
 	}
 
 	@Override
 	public void testPeriodic() {
-		countL = leftEnc.get();
-		countR = rightEnc.get();
-		distL = leftEnc.getDistance(); // returns number of rotations of wheel shaft (adjusted by the k4X in the
-										// constructor)
-		distR = rightEnc.getRaw(); // returns number of rotations of the encoder shaft
-		stoppedL = leftEnc.getStopped();
-		stoppedR = rightEnc.getStopped();
+		if (testPeriodicFunctions) {
+			// Write testPeriodic code in this loop
 
-		if (tester.getRawButton(GTA) && !tester.getRawButton(arcade)) {
-			gtaActive = true;
-		}
-		if (!tester.getRawButton(GTA) && tester.getRawButton(arcade)) {
-			gtaActive = true;
-		}
+			// Encoder value testing
+			countL = leftEnc.get();
+			countR = rightEnc.get();
+			distL = leftEnc.getDistance(); // returns number of rotations of wheel shaft (adjusted by the k4X in the
+											// constructor)
+			distR = rightEnc.getRaw(); // returns number of rotations of the encoder shaft
+			stoppedL = leftEnc.getStopped();
+			stoppedR = rightEnc.getStopped();
 
-		if (gtaActive) {
-
-			if (driver.getRawAxis(R2) != 0 && driver.getRawAxis(L2) == 0) {
-				drive.arcadeDrive(driver.getRawAxis(R2), driver.getRawAxis(LX), true);
-			} else if (driver.getRawAxis(L2) != 0 && driver.getRawAxis(R2) == 0) {
-				drive.arcadeDrive(-driver.getRawAxis(L2), driver.getRawAxis(LX), true);
-			} else {
-				drive.arcadeDrive(0, driver.getRawAxis(LX));
+			// Logic for switching between driving modes (GTA-style or arcade drive)
+			// TODO: Decide which mode driver likes better
+			if (tester.getRawButton(GTA) && !tester.getRawButton(arcade)) {
+				gtaActive = true;
+			}
+			if (!tester.getRawButton(GTA) && tester.getRawButton(arcade)) {
+				gtaActive = true;
 			}
 
-		} else {
+			if (gtaActive) {
 
-			drive.arcadeDrive(driver.getRawAxis(LY), driver.getRawAxis(RX), true);
+				if (driver.getRawAxis(R2) != 0 && driver.getRawAxis(L2) == 0) {
+					drive.arcadeDrive(driver.getRawAxis(R2), driver.getRawAxis(LX), true);
+				} else if (driver.getRawAxis(L2) != 0 && driver.getRawAxis(R2) == 0) {
+					drive.arcadeDrive(-driver.getRawAxis(L2), driver.getRawAxis(LX), true);
+				} else {
+					drive.arcadeDrive(0, driver.getRawAxis(LX));
+				}
 
+			} else {
+				drive.arcadeDrive(driver.getRawAxis(LY), driver.getRawAxis(RX), true);
+			}
+
+			Timer.delay(0.01);
+
+			// TODO Figure out where these values show up in the SmartDashboard
+			SmartDashboard.putNumber("LeftEncoder", distL);
+			SmartDashboard.putNumber("RightEncoder", distR);
+			LiveWindow.add(leftEnc);
+			LiveWindow.add(rightEnc);
 		}
-
-		Timer.delay(0.04);
-
-		// TODO Figure out where these values show up in the SmartDashboard
-		SmartDashboard.putNumber("LeftEncoder", distL);
-		SmartDashboard.putNumber("RightEncoder", distR);
-		LiveWindow.add(leftEnc);
-		LiveWindow.add(rightEnc);
 	}
 }
